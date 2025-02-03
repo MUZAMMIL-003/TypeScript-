9f45b12d420fe3376e354f1282ff2f69a3c34e3e
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-03 10:37:44 -0800

    .

--- index.ts
+++ index.ts
@@ -1,34 +1,34 @@
 all types of variables in typescript by copilot
-let num : number = 1;
-let str : string = "Hello";
-let bool : boolean = true;
-let obj : object = {first: "obj"};
-let arr : number[] = [1, 2, 3];
-let arr2 : Array<number> = [1, 2, 3];
-let tuple : [string, number] = ["tuple", 1];
-let any : any = 1;
-let unknown : unknown = 1;
-let nul : null = null;
-let und : undefined = undefined;
-let voidType : void = undefined;
-let never : never = (() => {throw new Error("error")})();
-let func : Function = () => {};
+let num: number = 1;
+let str: string = "Hello";
+let bool: boolean = true;
+let obj: object = { first: "obj" };
+let arr: number[] = [1, 2, 3];
+let arr2: Array<number> = [1, 2, 3];
+let tuple: [string, number] = ["tuple", 1];
+let any: any = 1;
+let unknown: unknown = 1;
+let nul: null = null;
+let und: undefined = undefined;
+let voidType: void = undefined;
+let never: never = (() => { throw new Error("error") })();
+let func: Function = () => { };
 func();
-let func2 : (a: number, b: number) => number = (a, b) => a + b;
+let func2: (a: number, b: number) => number = (a, b) => a + b;
 func2(1, 2);
-let func3 : (a: number, b: number) => number = function(a, b) {return a + b};
+let func3: (a: number, b: number) => number = function (a, b) { return a + b };
 func3(1, 2);
 
 
 interface User {
     name: string;
     age: number | string; /// it can be number or string
-    phoneNum? : string; //// optional
-    favNums : number[];
-    favDishes : string[];
+    phoneNum?: string; //// optional
+    favNums: number[];
+    favDishes: string[];
 }
 
-const user : User = {
+const user: User = {
     name: "John",
     age: 20,
     phoneNum: "010-1234-5678",
@@ -51,13 +51,31 @@ console.log(user);
 interface User1<Type> {
     name: string;
     age: number;
-    phoneNum? : string; //// optional
-    favNums : Type;
+    phoneNum?: string; //// optional
+    favNums: Type;
 }
 
-const user1 : User1 <number> = {
+const user1: User1<number> = {
     name: "John",
     age: 20,
     favNums: 123,
 }
 console.log(user1);
+
+const user2: User1<number[]> = {
+    name: "John",
+    age: 20,
+    favNums: [1, 2, 3],
+}
+console.log(user2);
+
+const user3: User1<{ one: string, two: number, three: boolean }> = {
+    name: "John",
+    age: 20,
+    favNums: {
+        one: "1",
+        two: 2,
+        three: true
+    },
+}
+console.log(user3);
