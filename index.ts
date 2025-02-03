191ccf1494fb7efb1e66e48b8c108b5b169530a7
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-03 11:03:08 -0800

    .

--- index.ts
+++ index.ts
@@ -113,3 +113,12 @@ let colors: { color: string, colorNum: number }[] = [
         colorNum: colorStatus.YELLOW
     }
 ]
+
+
+
+// function k arguments ko type dena
+let sum = (a :number,b:number) :number =>{
+ return a + b; // agar hum chahtey hen ki return mlney wali value bhi number ho to hum uper parameter k baad number likh saktey hen 
+}
+
+ let result : number = sum(10,10);
